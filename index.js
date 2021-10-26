408043c75658e16b547979eaecd00b602db21e9b
Feroj7 <feroj.web@gmail.com>
2021-10-27 02:50:46 +0600

    genius car mechanics server done

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//user: mydbuser1
+//pass: bYeXPlcS5tSoIAfV
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gwxst.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db('carMechanics');
+        const servicesCollection = database.collection('services');
+
+        //GET API
+        app.get('/services', async(req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //GET Single Service
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log("hitting service with specific id", id);
+            const query = {_id:ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        //POST API
+        app.post('/services', async(req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        })
+
+        //DELETE API
+        app.delete('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Genius Server Running')
+})
+
+app.listen(port, () => {
+    console.log("Genius Server Running on port", port);
+})
